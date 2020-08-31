21b065457e67745b6bf174cc595505da8c424f68
Erik Langenhan <erik.langenhan@web.de>
2020-08-31 14:11:36 +0200

    switch to different category view on smaller screens

--- src/app/article-card/article-card.component.ts
+++ src/app/article-card/article-card.component.ts
@@ -14,6 +14,7 @@ export class ArticleCardComponent implements OnInit {
   @Input() date;
   @Input() category;
   @Input() index;
+  @Input() id;
 
   constructor(private matDialog: MatDialog) { }
 
--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -8,11 +8,13 @@ export class DataService {
 
   constructor(private httpClient: HttpClient) { }
 
-  public getData(category = ""){
+  public getData(category?: string) {
+    if (!category) category = ""
     return this.httpClient.get(`https://cms.factsforfriends.de/facts?category_contains=${category.toLowerCase()}`)
   }
 
-  public search(query: string, category = "", limit?: number) {
+  public search(query: string, category?: string, limit?: number) {
+    if (!category) category = ""
     // connect query with category using AND does not work with strapi.io at the moment, future version of strapi will support this
     return this.httpClient.get(`https://cms.factsforfriends.de/facts?_q=${query}&category_contains=${category.toLowerCase()}&_limit=${limit}`)
   }
