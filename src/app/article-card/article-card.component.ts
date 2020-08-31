461b7f35323782c094d9db23068b3ef510f5e6a6
Erik Langenhan <erik.langenhan@web.de>
2020-08-31 15:28:45 +0200

    truncate text

--- src/app/article-card/article-card.component.ts
+++ src/app/article-card/article-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { ShareMenuComponent } from '../share-menu/share-menu.component';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 
@@ -7,7 +7,8 @@ import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
   templateUrl: './article-card.component.html',
   styleUrls: ['./article-card.component.scss']
 })
-export class ArticleCardComponent implements OnInit {
+export class ArticleCardComponent implements OnInit, AfterViewInit {
+  @ViewChild('textElement') textElement: ElementRef
   @Input() title;
   @Input() text;
   @Input() url;
@@ -16,17 +17,36 @@ export class ArticleCardComponent implements OnInit {
   @Input() index;
   @Input() id;
 
+  isOverflown: boolean = false
+
   constructor(private matDialog: MatDialog) { }
 
   ngOnInit(): void {
   }
 
+  ngAfterViewInit(): void {
+    this.isOverflown = this.checkIfOverflown(this.textElement.nativeElement)
+  }
+
   share(): void {
-    const dialogConfig = new MatDialogConfig();
-    dialogConfig["data"] = {
-      url: this.url
+    if (navigator.share) {
+      navigator.share({
+          title: this.title,
+          text: this.text,
+          url: 'https://factsforfriends.de/snack/' + this.id
+        }).then(() => console.log('Successful share'))
+        .catch(error => console.log('Error sharing:', error));
+    } else {
+      const dialogConfig = new MatDialogConfig();
+      dialogConfig["data"] = {
+        url: this.url
+      }
+      const dialogRef = this.matDialog.open(ShareMenuComponent, dialogConfig);
     }
-    const dialogRef = this.matDialog.open(ShareMenuComponent, dialogConfig);
+  }
+
+  checkIfOverflown(element): boolean {
+    return element.scrollHeight > element.clientHeight || element.scrollWidth > element.clientWidth
   }
 
 }
