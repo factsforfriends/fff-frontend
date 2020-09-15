39cdbc768dd8f340f302e30fec6b5786e7962c28
Erik Langenhan <eriklangenhan@gmail.com>
2020-09-15 16:28:45 +0200

    fix debug

--- src/app/article-card/article-card.component.ts
+++ src/app/article-card/article-card.component.ts
@@ -24,7 +24,7 @@ export class ArticleCardComponent implements OnInit, AfterViewInit {
   constructor(private matDialog: MatDialog) { }
 
   ngOnInit(): void {
-    this.share();
+
   }
 
   ngAfterViewInit(): void {
