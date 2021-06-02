d870aeebd2919c2f35699da555bf88414c7cb662
PotSchmoell <patrick96mueller@gmail.com>
2021-06-02 14:51:35 +0200

    Preventing navigator sharing on desktop and fixed opera error

--- src/app/sharebutton/sharebutton.component.ts
+++ src/app/sharebutton/sharebutton.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { ShareMenuComponent } from '../share-menu/share-menu.component';
+import { DeviceDetectorService } from 'ngx-device-detector';
 
 @Component({
   selector: 'app-sharebutton',
@@ -17,20 +18,28 @@ export class SharebuttonComponent implements OnInit {
   @Input() id: string;
   @Input() image_url: string;
 
-  constructor(private matDialog: MatDialog) { }
+  isMobile:boolean = false; //initiate as false
+  deviceInfo = null;
+
+  constructor(private matDialog: MatDialog, private deviceService: DeviceDetectorService) { }
 
   ngOnInit(): void {
+    this.deviceInfo = this.deviceService.getDeviceInfo();
+      const isMobile = this.deviceService.isMobile();
+      const isTablet = this.deviceService.isTablet();
+      const isDesktopDevice = this.deviceService.isDesktop();
+
   }
 
   share(): void {
-    // if (navigator.share) {
-    //   navigator.share({
-    //       title: this.title,
-    //       text: this.truncateChar(this.title + '\n' + this.text),
-    //       url: 'https://factsforfriends.de/fact/' + this.id
-    //     }).then(() => console.log('Successful share'))
-    //     .catch(error => console.log('Error sharing:', error));
-    // } else {
+    if (navigator.share && (this.deviceService.isMobile() || this.deviceService.isTablet())) {
+      navigator.share({
+          title: this.title,
+          text: this.truncateChar(this.title + '\n' + this.text),
+          url: 'https://factsforfriends.de/fact/' + this.id
+        }).then(() => console.log('Successful share'))
+        .catch(error => console.log('Error sharing:', error));
+    } else {
       const dialogConfig = new MatDialogConfig();
       
       dialogConfig["data"] = {
@@ -40,8 +49,9 @@ export class SharebuttonComponent implements OnInit {
         sharepic: this.image_url
       }
       const dialogRef = this.matDialog.open(ShareMenuComponent, dialogConfig);
-    // }
+    }
   }
+
 
   truncateChar(text: string, limit: number = 280): string {
     if(!text || text.length <= limit )
@@ -53,5 +63,4 @@ export class SharebuttonComponent implements OnInit {
     let short_until = without_html.substring(0, limit).lastIndexOf(" ");
     return without_html.substring(0, short_until)+ " ..."; 
   }
-
 }
