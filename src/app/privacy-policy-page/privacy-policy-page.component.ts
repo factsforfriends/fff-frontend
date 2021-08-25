2dee106b5304cde838e1ee2088dc45e770840368
PotSchmoell <patrick96mueller@gmail.com>
2021-08-25 12:25:09 +0200

    - Updated privacy policy text - Implemented tracking button

--- src/app/privacy-policy-page/privacy-policy-page.component.ts
+++ src/app/privacy-policy-page/privacy-policy-page.component.ts
@@ -4,15 +4,27 @@ import { Title, Meta } from '@angular/platform-browser';
 @Component({
   selector: 'app-privacy-policy-page',
   templateUrl: './privacy-policy-page.component.html',
-  styleUrls: ['./privacy-policy-page.component.scss']
+  styleUrls: ['./privacy-policy-page.component.scss'],
 })
 export class PrivacyPolicyPageComponent implements OnInit {
+  tracking: boolean;
 
   constructor(private titleService: Title, private metaService: Meta) {
-    this.titleService.setTitle('Datenschutz | Facts for Friends')
+    this.titleService.setTitle('Datenschutz | Facts for Friends');
   }
 
   ngOnInit(): void {
+    this.tracking = true;
   }
 
+  toggle_tracking() {
+    this.tracking = !this.tracking;
+    if (this.tracking) {
+      let paq = window['_paq'];
+      paq.push(['forgetUserOptOut']);
+    } else {
+      let paq = window['_paq'];
+      paq.push(['optUserOut']);
+    }
+  }
 }
